c6c6ef94e7375fa1552b5399ae0f891fb7610eeb
Virgile <vthevenon29@outlook.fr>
2024-05-03 13:58:30 +0200

    remove Hello

--- src/main/java/project/energy/App.java
+++ src/main/java/project/energy/App.java
@@ -1,18 +1,17 @@
 package project.energy;
 
-import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class App extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("menu.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 720, 480);
+        stage.setTitle("");
         stage.setScene(scene);
         stage.show();
     }
