854639bb34790469f13f33544929d2d69bc5e14f
Virgile <vthevenon29@outlook.fr>
2024-05-03 14:10:19 +0200

    css + image

--- src/main/java/project/energy/App.java
+++ src/main/java/project/energy/App.java
@@ -5,12 +5,15 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class App extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("menu.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 720, 480);
+        Scene scene = new Scene(fxmlLoader.load(), 1200, 630);
+        scene.getStylesheets().addAll(Objects.requireNonNull(this.getClass().getResource("css/style.css")).toExternalForm());
+        stage.setResizable(false);
         stage.setTitle("");
         stage.setScene(scene);
         stage.show();
