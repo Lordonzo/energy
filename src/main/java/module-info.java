1a5e14c3462d364e976e1e2d965776c7b681d9b7
Virgile <vthevenon29@outlook.fr>
2024-05-03 13:35:47 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module project.energy {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens project.energy to javafx.fxml;
+    exports project.energy;
+}
