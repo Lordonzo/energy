a24a740e4cc19ab34ba377d9fc5958135e3dc7b3
Lordonzo <vthevenon29@outlook.fr>
2024-05-07 11:10:54 +0200

    css + carte begin

--- src/main/java/project/energy/controller/CarteController.java
+++ src/main/java/project/energy/controller/CarteController.java
@@ -1,4 +1,13 @@
 package project.energy.controller;
 
-public class CarteController {
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CarteController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
